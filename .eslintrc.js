f186d27590ce53b85c51ec2a62070616e45fe3ec
farhan-shikalgar <farhanshikalgar@gmail.com>
2023-07-19 15:18:14 +0530

    openssl

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,6 +20,7 @@ module.exports = {
   plugins: [
     'react',
   ],
+  
   rules: {
     "react/no-unescaped-entities": "off",
     "@next/next/no-page-custom-font": "off",
@@ -84,4 +85,5 @@ module.exports = {
       },
     ],
   },
+  
 };
